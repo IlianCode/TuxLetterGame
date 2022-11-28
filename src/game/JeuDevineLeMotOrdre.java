7db4c36c79f63769a50d1b2345f1ab01f36963fe
IlianCode <benaissailian@outlook.fr>
2022-11-28 22:42:49 +0100

    jeu fonctionnel, avancement des fonction dans JeuDevineLeMotOrdre

--- src/game/JeuDevineLeMotOrdre.java
+++ src/game/JeuDevineLeMotOrdre.java
@@ -11,16 +11,53 @@ package game;
 public class JeuDevineLeMotOrdre extends Jeu{
     private int nbLettresRestantes;
     private Chronometre chrono; 
-    
+    int nbLetterLeft;
     public JeuDevineLeMotOrdre(){
         super();
     }
+    
+    @Override
+    protected void démarrePartie(Partie partie){
+        //initialisation du chrono 
+        int limiteChrono = 20;
+        chrono = new Chronometre(limiteChrono);
+        chrono.start();
+        chrono.stop();
+        
+        //initilisation du nombre de lettre restante dans la partie 
+        nbLetterLeft = partie.getMot().length();
+    }
 
-    private Boolean tuxTrouveLettre(){
+    private Boolean tuxTrouveLettre(int i){
                 //valeur par défaut 
-        Boolean trouvee = coli(lettres.get(0));
+        Boolean trouvee = coli(lettres.get(i));
         return trouvee;
         
+    }
+
+    @Override
+    protected void appliqueRegles(Partie partie) {
+        int lettreIndice;
+        
+        //s'il reste du temps dans la partie 
+        if(chrono.remainsTime()){
+            lettreIndice = partie.getMot().length() - nbLetterLeft;
+            //si lettre trouver dans l'ordre le nombre de lettre restantre diminue 
+            if(tuxTrouveLettre(lettreIndice)&& (lettreIndice < partie.getMot().length()) ){
+                env.removeObject(lettres.get(lettreIndice));
+                nbLettresRestantes -=1;
+            }
+            if(nbLettresRestantes == 0) {
+                partie.setEnd(true);
+            }
+        }else {
+            partie.setEnd(true);
+        }
+    }
+
+    @Override
+    protected void terminePartie(Partie partie) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
 
